4d04ef635685d9fbec78b7419f7e77b3a694960f
Chilou33 <148858941+Chilou33@users.noreply.github.com>
2024-11-17 16:47:04 +0100

    Ajout d'un fichier pour initialiser et exécuter une application Pyxel avec une fonction de dessin

---
+++ piéces.py
@@ -0,0 +1,13 @@
+import pyxel
+def update():
+    pass
+
+def draw():
+    pyxel.cls(0)
+    pyxel.blt(80, 60, 1, 0, 0, 16, 16, 0)
+    
+    
+    
+pyxel.init(160, 120)
+pyxel.load("my_resource.pyxres")
+pyxel.run(update, draw)
