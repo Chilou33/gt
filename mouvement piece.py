786765692b83077e33b9fbb50ae1be943943df11
ToutzCA <camille.toutzevitch@gmail.com>
2025-02-10 23:45:59 +0100

    update fonction rotation (pas terminée)

--- mouvement piece.py
+++ mouvement piece.py
@@ -26,9 +26,9 @@ def piece1():
              [1],
              [1]]
     for Py in piece:
-        for Px in range(len(Py)):
+        for Px in Py:
             if Px!=0:
-                piece[piece.index(Py)][Px] = [piece.index(Py), Px]
+                piece[piece.index(Py)][Py.index(Px)] = [piece.index(Py),Py.index(Px)]
     return piece
        
 def piece2(): 
@@ -66,46 +66,74 @@ plateau = [[0 for Tx in range (0,12)]for Ty in range(0,5)]
 def deplacement(sens:str,norme:int,piece:list):
     plateau = [[0 for Tx in range (0,12)]for Ty in range(0,5)]
     print(plateau)
+    nouvPiece=piece.copy()
     if sens=='g':
-        for Py in piece:
+        for Py in nouvPiece:
             for Px in Py:
                 Px[1]-=norme
     if sens=='d':
-        for Py in piece:
+        for Py in nouvPiece:
             for Px in Py:
                 Px[1]+=norme
     if sens=='b':
-        for Py in piece:
+        for Py in nouvPiece:
             for Px in Py:
                 Px[0]+=norme
     if sens=='h':
-        for Py in piece:
+        for Py in nouvPiece:
             for Px in Py:
                 Px[0]-=norme
-    for Py in piece:
+    for Py in nouvPiece:
         for Px in Py: 
                 plateau[Px[0]][Px[1]] = 1
-    return plateau,piece
+    return plateau,nouvPiece
 
-def rotation(piece,plateau,origine):
-    translation =[-origine[0],-origine[1]]
-    for ligne in piece:
+def rotation(plateau):
+    piece_coordinates=[]
+    for i, plateau_row in enumerate(plateau):
+        for j, piece_coordinate in enumerate(plateau_row):
+            if piece_coordinate == 1:
+                piece_coordinates.append([i, j])
+    print(piece_coordinates)
+    nouvPiece=[]
+    #origin = piece_coordinates[0][0]
+    for ligne in piece_coordinates:
         for coordonnées in ligne:
-            coordonnées[0]+=translation[0]
-            coordonnées[1]+=translation[1]
-    origine[0]+=translation[0]
-    origine[1]+=translation[1]
-    nouvPiece = []
-    for ligne in piece:
-        for coordonnées in ligne:
-            nouvCoo=["",""]
-            nouvCoo[0] -= coordonnées[1]
-            nouvCoo[1] = coordonnées[0]
-            nouvCoo[0] -= translation[0]
-            nouvCoo[1] -= translation
-            nouvPiece.append(nouvCoo)
-            plateau[nouvCoo[0]][nouvCoo[1]]=1
-        return plateau
+            print(type(coordonnées))
+            nouvCo=[]
+            nouvCo.append(-coordonnées[1])
+            nouvCo.append(coordonnées[0])
+            
+            print(nouvCo)
+            nouvPiece.append(nouvCo)
+            plateau[nouvCo[0]][nouvCo[1]]=1
+    print(nouvPiece)
+    return plateau
+
+plateau1=  [[0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    # translation =[-origine[0],-origine[1]]
+    # for ligne in piece:
+    #     for coordonnées in ligne:
+    #         coordonnées[0]+=translation[0]
+    #         coordonnées[1]+=translation[1]
+    # origine[0]+=translation[0]
+    # origine[1]+=translation[1]
+    # nouvPiece = []
+    # for ligne in piece:
+    #     for coordonnées in ligne:
+    #         nouvCoo=["",""]
+    #         nouvCoo[0] -= coordonnées[1]
+    #         nouvCoo[1] = coordonnées[0]
+    #         nouvCoo[0] -= translation[0]
+    #         nouvCoo[1] -= translation
+    #         nouvPiece.append(nouvCoo)
+    #         plateau[nouvCoo[0]][nouvCoo[1]]=1
+    #     return plateau
 
 
 
