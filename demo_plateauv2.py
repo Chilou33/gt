fd03fc4dfaa1a0546309c1a6925c56adb5e428a1
ToutzCA <camille.toutzevitch@gmail.com>
2025-03-13 12:01:10 +0100

    demo v2

--- demo_plateauv2.py
+++ demo_plateauv2.py
@@ -13,7 +13,6 @@ class KataminoBoard:
 
         self.colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-        # Create a piece
         self.piece = Piece(1, [[1, 1], [1], [1], [1]], self.board)
 
         pyxel.run(self.update, self.draw)
@@ -24,12 +23,14 @@ class KataminoBoard:
         if pyxel.btnp(pyxel.KEY_P):
             self.board = self.piece.place_on_plateau()
         if pyxel.btnp(pyxel.KEY_LEFT):
-            self.board = self.piece.deplacement(0, -1)
+            self.board = self.piece.deplacement(-1, 0)
         if pyxel.btnp(pyxel.KEY_RIGHT):
-            self.board = self.piece.deplacement(0, 1)
-        if pyxel.btnp(pyxel.KEY_DOWN):
             self.board = self.piece.deplacement(1, 0)
+        if pyxel.btnp(pyxel.KEY_DOWN):
+            self.board = self.piece.deplacement(0, 1)
         if pyxel.btnp(pyxel.KEY_UP):
+            self.board = self.piece.deplacement(0, -1)
+        if pyxel.btnp(pyxel.KEY_R):
             self.board = self.piece.rotate()
 
     def draw(self):
@@ -76,47 +77,49 @@ class Piece:
         return self.plateau
 
     def deplacement(self, dy, dx):
-        # Clear the current position of the piece on the plateau
+
         for x, y in self.actual_coordinates:
             if 0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]):
                 self.plateau[x][y] = 0
 
-        # Update the coordinates
         new_coordinates = []
         for x, y in self.actual_coordinates:
             new_x, new_y = x + dx, y + dy
             if 0 <= new_x < len(self.plateau) and 0 <= new_y < len(self.plateau[0]):
                 new_coordinates.append([new_x, new_y])
-        self.actual_coordinates = new_coordinates
+            else:
+                pass
+        if len(new_coordinates)==5:
+            self.actual_coordinates = new_coordinates
+        else:
+            pass
 
-        # Place the piece at the new position
         return self.place_on_plateau()
 
     def rotate(self):
-        # Clear the current position of the piece on the plateau
         for x, y in self.actual_coordinates:
             if 0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]):
                 self.plateau[x][y] = 0
 
-        # Find the top-left corner of the piece
         min_x = min(coord[0] for coord in self.actual_coordinates)
         min_y = min(coord[1] for coord in self.actual_coordinates)
 
-        # Translate coordinates to the origin
         translated_coordinates = [[x - min_x, y - min_y] for x, y in self.actual_coordinates]
+         
 
-        # Apply rotation (90 degrees clockwise)
         rotated_coordinates = [[y, -x] for x, y in translated_coordinates]
 
-        # Translate coordinates back to original position
-        self.actual_coordinates = [[x + min_x, y + min_y] for x, y in rotated_coordinates]
+        self.actual_coordinates = [[x + min_x, y + min_y] for x, y in rotated_coordinates] #if 0 <= x+min_x < len(self.plateau) and 0 <= y+min_y < len(self.plateau[0])]
+        
 
-        # Place the piece at the new position
         return self.place_on_plateau()
 
 def create_pieces(plateau):
     piece = [
-        Piece(1, [[1, 1], [1], [1], [1]], plateau),
+        Piece(1, [[1, 1],
+                                [1], 
+                                [1], 
+                                [1]], plateau),
         Piece(2, [[0, 2], [2, 2, 2], [0, 2]], plateau),
         Piece(3, [[3], [3, 3, 3], [0, 3]], plateau),
         Piece(4, [[4], [4, 4], [4, 4]], plateau),
@@ -136,8 +139,6 @@ def plateau_clear():
 
     return plateau
 
-# Initialize plateau
 plateau = [[0 for _ in range(12)] for _ in range(5)]
 
-# Create and run the game
 KataminoBoard(plateau)
