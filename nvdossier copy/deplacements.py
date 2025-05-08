7f8ee3123eda6dc9b39b05d76d758bcfeff4b40b
ToutzCA <camille.toutzevitch@gmail.com>
2025-05-08 19:16:25 +0200

    la version la plus aboutie est maintenant deplacements.py les deplacements sont maintenant completement fonctionnels

--- nvdossier copy/deplacements.py
+++ nvdossier copy/deplacements.py
@@ -14,7 +14,7 @@ class App:
 class MainMenu:
     def __init__(self):
         self.message = "Bienvenue dans Pythominoes\nAppuyez sur Entree pour jouer"
-        pyxel.load("tilemap.pyxres")
+        pyxel.load("ressources.pyxres")
         self.pieces_cascade_liste = []  # Liste des pièces en cascade
         self.val = randint(1, 12) * 16 + 8  # Valeur initiale
         
@@ -67,7 +67,7 @@ class EcranChoixPieces:
         self.liste_pieces_deja_choisies = pieces_selectionnees
         self.liste_piece_choisies = []
         self.position_curseur = 0
-        pyxel.load("tilemap.pyxres")
+        pyxel.load("ressources.pyxres")
         self.etape =len(pieces_selectionnees)
 
     def update(self):
@@ -122,7 +122,7 @@ class EcranChoixPieces:
 class Ecran_de_victoire:
     def __init__(self):
         self.message = "Victoire!"
-        pyxel.load("tilemap.pyxres")
+        pyxel.load("ressources.pyxres")
         self.pieces_cascade_liste = []  # Liste des pièces en cascade [x, y, image_val, angle_rad, speed]
         self.val = randint(1, 12) * 16 + 8  # Valeur initiale
         self.piece_size = 32 # Piece size after scaling (16 * 2.0)
@@ -194,15 +194,17 @@ taille = 12
 plateau = Plateau(taille).clear 
 
 class KataminoBoard:
-    def __init__(self, plateau,cell_size=32):
+    def __init__(self, plateau, cell_size=32):
+        self.Dplateau = [row[:] for row in plateau]
         self.plateau = plateau
+
         self.cell_size = cell_size
         self.ligne = len(plateau)
         self.cols = len(plateau[0]) if self.ligne > 0 else 0
         pyxel.colors.from_list([0x000000, 0xFFFFFF, 0x7F7F7F, 0xC3C3C3, 0x64BCED, 0x200CFF, 0xFF1E27, 0x880015, 0xFFFF00, 0xF58B1A, 0x20BD0F, 0x104F12, 0xF585B1, 0xCA42D1, 0x6325D4, 0x807625])
         self.colors = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     
-        pyxel.load("tilemap.pyxres")
+        pyxel.load("ressources.pyxres")
 
         self.pieces = create_pieces(self.plateau)
         self.selected_piece_index = 0
@@ -231,8 +233,14 @@ class KataminoBoard:
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
+        if pyxel.btnp(pyxel.KEY_A):
+            self.selected_piece.retirer()
+
         if pyxel.btnp(pyxel.KEY_P,repeat=10):
-            self.plateau = self.selected_piece.place_on_plateau()
+            self.plateau, success = self.selected_piece.place_on_plateau()
+            if not success :
+                self.alert_message = "Placement impossible!"
+                self.alert_timer = self.alert_duration
 
         if self.verif_victoire():
             self.alert_message = "Victoire!"
@@ -240,51 +248,51 @@ class KataminoBoard:
             App(Ecran_de_victoire())
 
         if pyxel.btnp(pyxel.KEY_R,repeat=10):
-            self.plateau, success = self.selected_piece.rotate()
+            self.Dplateau, success = self.selected_piece.rotate()
             if not success:
                 self.alert_message = "Rotation impossible!"
                 self.alert_timer = self.alert_duration
 
         if pyxel.btnp(pyxel.KEY_E,repeat=8):
-            self.plateau, success = self.selected_piece.symetrie()
+            self.Dplateau, success = self.selected_piece.symetrie()
             if not success:
                 self.alert_message = "Symetrie impossible!"
                 self.alert_timer = self.alert_duration
 
         if pyxel.btnp(pyxel.KEY_LEFT,repeat=8):
-            self.plateau, success = self.selected_piece.deplacement(-1, 0)
+            self.Dplateau, success = self.selected_piece.deplacement(-1, 0)
             if not success:
                 self.alert_message = "Deplacement impossible!"
                 self.alert_timer = self.alert_duration
 
         if pyxel.btnp(pyxel.KEY_RIGHT,repeat=8):
-            self.plateau, success = self.selected_piece.deplacement(1, 0)
+            self.Dplateau, success = self.selected_piece.deplacement(1, 0)
             if not success:
                 self.alert_message = "Deplacement impossible!"
                 self.alert_timer = self.alert_duration
 
         if pyxel.btnp(pyxel.KEY_DOWN,repeat=8):
-            self.plateau, success = self.selected_piece.deplacement(0, 1)
+            self.Dplateau, success = self.selected_piece.deplacement(0, 1)
             if not success:
                 self.alert_message = "Deplacement impossible!"
                 self.alert_timer = self.alert_duration
 
         if pyxel.btnp(pyxel.KEY_UP,repeat=8):
-            self.plateau, success = self.selected_piece.deplacement(0, -1)
+            self.Dplateau, success = self.selected_piece.deplacement(0, -1)
             if not success:
                 self.alert_message = "Deplacement impossible!"
                 self.alert_timer = self.alert_duration
 
         if pyxel.btnp(pyxel.KEY_N):       
-            # Changer l'index de la pièce sélectionnée
-            self.selected_piece_index = (self.selected_piece_index + 1) % len(self.pieces)
-            self.selected_piece = self.pieces[self.selected_piece_index]
+            self.plateau,success = self.selected_piece.place_on_plateau()
+            if success :
+                self.selected_piece_index = (self.selected_piece_index + 1) % len(self.pieces)
+                self.selected_piece = self.pieces[self.selected_piece_index]
+
         
         if pyxel.btn(pyxel.KEY_G):
             App(EcranChoixPieces())
-        # for piece in self.pieces_placees:
-        #     piece.place_on_plateau()
-        # Mettre à jour le timer d'alerte
+        
         if self.alert_timer > 0:
             self.alert_timer -= 1
         
@@ -310,7 +318,18 @@ class KataminoBoard:
                     self.cell_size,
                     2
                 )
-
+        for y in range(self.ligne):
+            for x in range(self.cols):
+                value = self.Dplateau[y][x]
+                if value > 0:
+                    color = self.colors[(value % len(self.colors))-1]
+                    pyxel.rect(
+                        (x * self.cell_size)+3,
+                        (y * self.cell_size)+3,
+                        self.cell_size-6,
+                        self.cell_size-6,
+                        color
+                    )
         # Draw piece selection area
         pyxel.text(10, self.ligne * self.cell_size + 10, "Piece selectionnee :", 0)
         #self.liste_des_coordonnees_des_boutons = [(32*3,32*6),(32*4,32*6),(32*5,32*6),(32*6,32*6),(32*7,32*6),(32*8,32*6),(32*3,32*7),(32*4,32*7),(32*5,32*7),(32*6,32*7),(32*7,32*7),(32*8,32*7)]
@@ -329,11 +348,11 @@ class Piece:
     def __init__(self, numero, patron, plateau):
         self.numero = numero
         self.patron = patron
-
         self.plateau = plateau
-        self.Dplateau = plateau
-        
-        self.actual_coordinates = self.convert_to_coordinates()
+        # Créer une copie indépendante du plateau
+        self.Dplateau = [row[:] for row in plateau]
+        self.etat_deplacement = False
+        self.cos_actuelles = self.convert_to_coordinates()
 
     def convert_to_coordinates(self):
         coordinates = []
@@ -343,91 +362,108 @@ class Piece:
                     coordinates.append([i, j])
         return coordinates
 
-    def place_on_plateau(self):
-        # if self.actual_coordinates not in KataminoBoard.pieces_placées :
-        #             KataminoBoard.pieces_placees.append(self.actual_coordinates)
-        for x, y in self.actual_coordinates:
-            if 0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]):
-                self.plateau[x][y] = self.numero
-        return self.plateau
+    def place_on_Dplateau(self):
+        for i in range(len(self.Dplateau)):
+            for j in range(len(self.Dplateau[0])):
+                self.Dplateau[i][j] = 0
 
-    def deplacement(self, dy, dx):
-        # Sauvegarde de l'état actuel pour restauration en cas d'échec
-        old_coordinates = self.actual_coordinates.copy()
-        
-        # Effacer la pièce actuelle du plateau
-        for x, y in self.actual_coordinates:
-            if 0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]):
+        if self.etat_deplacement == True:
+            for x, y in self.cos_actuelles:
+                self.Dplateau[x][y] = self.numero
+
+        if self.etat_deplacement == False:
+            for x, y in self.cos_actuelles:
                 self.plateau[x][y] = 0
+                self.Dplateau[x][y] = self.numero
+                self.etat_deplacement = True
+        return self.Dplateau
+    
+    def place_on_plateau(self):
+        if self.etat_deplacement == True:
+            if all(self.plateau[new_x][new_y] == 0 for new_x, new_y in self.cos_actuelles):
+                for x, y in self.cos_actuelles:
+                    self.Dplateau[x][y] = 0
+                    self.plateau[x][y] = self.numero
+                    self.etat_deplacement = False
+            else : return self.plateau, False
+        if self.etat_deplacement == False:
+            for x, y in self.cos_actuelles:
+                self.plateau[x][y] = self.numero
+                self.etat_deplacement = False
+        return self.plateau,True
+
+    def retirer(self):
+        if self.etat_deplacement == True:
+            for x,y in self.cos_actuelles :
+                self.Dplateau[x][y] = 0
+        else :
+            for x,y in self.cos_actuelles :
+                self.plateau[x][y] = 0
+        self.cos_actuelles = self.convert_to_coordinates()
+        
+    def deplacement(self, dy, dx):
+        self.place_on_Dplateau()
+        # Sauvegarde de l'état actuel pour restauration en cas d'échec
+        old_coordinates = self.cos_actuelles.copy()
 
         # Calculer les nouvelles coordonnées
         new_coordinates = []
-        for x, y in self.actual_coordinates:
+        for x, y in self.cos_actuelles:
             new_x, new_y = x + dx, y + dy
             new_coordinates.append([new_x, new_y])
         
         # Vérifier si les nouvelles coordonnées sont valides et si les cases sont libres
-        if all(0 <= new_x < len(self.plateau) and 0 <= new_y < len(self.plateau[0]) 
-               for new_x, new_y in new_coordinates):
-            # Vérifier que toutes les cases cibles sont vides
-            if all(self.plateau[new_x][new_y] == 0 for new_x, new_y in new_coordinates):
-                self.actual_coordinates = new_coordinates
-                
-                return self.place_on_plateau(), True
+        if all(0 <= new_x < len(self.plateau) and 0 <= new_y < len(self.plateau[0]) for new_x, new_y in new_coordinates):
+            self.cos_actuelles = new_coordinates
+            return self.place_on_Dplateau(), True
         
         # Si le déplacement est impossible, restaurer l'état initial
-        self.actual_coordinates = old_coordinates
-        return self.place_on_plateau(), False
+        self.cos_actuelles = old_coordinates
+        return self.place_on_Dplateau(), False
 
     def rotate(self):
-        for x, y in self.actual_coordinates:
-            if 0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]):
-                self.plateau[x][y] = 0
+        self.place_on_Dplateau()
 
-        if self.numero in [6, 8,4]:
-            self.rotation_anchor = self.actual_coordinates[1]
+        if self.numero in [6,8,4]:
+            self.rotation_anchor = self.cos_actuelles[1]
         if self.numero in [1, 2, 3, 5, 7, 9, 10, 11, 12]:
-            self.rotation_anchor = self.actual_coordinates[2]
+            self.rotation_anchor = self.cos_actuelles[2]
 
         anchor_x = self.rotation_anchor[0]
         anchor_y = self.rotation_anchor[1]
 
-        translated_coordinates = [[x - anchor_x, y - anchor_y] for x, y in self.actual_coordinates]
+        translated_coordinates = [[x - anchor_x, y - anchor_y] for x, y in self.cos_actuelles]
         rotated_coordinates = [[y, -x] for x, y in translated_coordinates]
         final_coordinates = [[x + anchor_x, y + anchor_y] for x, y in rotated_coordinates]
 
         if all(0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]) for x, y in final_coordinates):
-            self.actual_coordinates = final_coordinates
-            return self.place_on_plateau(), True
+            self.cos_actuelles = final_coordinates
+            return self.place_on_Dplateau(), True
         else:
             # Remettre les pièces à leur position d'origine si la rotation est impossible
-            return self.place_on_plateau(), False
+            return self.place_on_Dplateau(), False
 
     def symetrie(self):
+        self.place_on_Dplateau()
         # Sauvegarde de l'état actuel
-        old_coordinates = self.actual_coordinates.copy()
+        old_coordinates = self.cos_actuelles.copy()
         
-        for x, y in self.actual_coordinates:
-            if 0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]):
-                self.plateau[x][y] = 0
 
-        max_y = max(y for x, y in self.actual_coordinates)
-        symetrie_coordinates = [[x, max_y - y] for x, y in self.actual_coordinates]
+        max_y = max(y for x, y in self.cos_actuelles)
+        symetrie_coordinates = [[x, max_y - y] for x, y in self.cos_actuelles]
 
-        min_y = min(y for x, y in self.actual_coordinates)
+        min_y = min(y for x, y in self.cos_actuelles)
         decalage = min_y - min(y for x, y in symetrie_coordinates)
 
         symetrie_coordinates = [[x, y + decalage] for x, y in symetrie_coordinates]
 
         if all(0 <= x < len(self.plateau) and 0 <= y < len(self.plateau[0]) for x, y in symetrie_coordinates):
-            # Vérifier que les cases cibles sont vides
-            if all(self.plateau[x][y] == 0 for x, y in symetrie_coordinates):
-                self.actual_coordinates = symetrie_coordinates
-                return self.place_on_plateau(), True
+            self.cos_actuelles = symetrie_coordinates
+            return self.place_on_Dplateau(), True
             
         # Restaurer l'état initial si la symétrie est impossible
-        self.actual_coordinates = old_coordinates
-        return self.place_on_plateau(), False
+        self.cos_actuelles = old_coordinates
+        return self.place_on_Dplateau(), False
 
 def create_pieces(plateau):
     pieces = [
